7bc9ebc51303aa2572b2bd4cb0a2da52d2515547
guest <bvishnu123vardhan@gmail.com>
2023-07-14 01:06:29 +0530

    v3/basicsofSpringBoot

--- springboot-rest-api/src/main/java/com/dailycodelearner/springboot/v3/controller/StudentController.java
+++ springboot-rest-api/src/main/java/com/dailycodelearner/springboot/v3/controller/StudentController.java
@@ -1,6 +1,7 @@
 package com.dailycodelearner.springboot.v3.controller;
 
 import com.dailycodelearner.springboot.v3.bean.Student;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -26,13 +27,7 @@ public class StudentController {
 
     @GetMapping("student/all")
     public List<Student> getStudents(){
-        return new ArrayList<>(Arrays.asList(
-                new Student(1,"vishnuvardhan","Bandlamudi"),
-                new Student(2,"munner","react"),
-                new Student(3,"deepa","node"),
-                new Student(4,"sajid","fullstack")
-        )
-        );
+        return students;
 
     }
 
@@ -50,5 +45,29 @@ public class StudentController {
         return students.stream().filter(s->s.getId()==id).findAny().orElse(
                 new Student(999,"default","default")
         );
+    }
+
+    @PostMapping("/student")
+    public int addStudent(@RequestBody Student student){
+        students.add(student);
+        return 1;
+    }
+
+    @PutMapping("/student")
+    public List<Student> updateStudent(@RequestBody Student student){
+        Student findStudent=students.stream().filter(s->s.getId()==student.getId()).findAny().get();
+        findStudent.setId(student.getId());
+        findStudent.setFirstName(student.getFirstName());
+        findStudent.setLastName(student.getLastName());
+
+        students.set(findStudent.getId()-1,findStudent);
+
+        return students;
+    }
+
+    @DeleteMapping("/student")
+    public ResponseEntity<List<Student>> deleteStudent(@RequestParam int id){
+        students.remove(id-1);
+        return ResponseEntity.ok(students);
     }
 }
