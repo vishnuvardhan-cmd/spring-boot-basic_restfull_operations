e5b6e0d136e709efb87609da194a46297e06b9a4
guest <bvishnu123vardhan@gmail.com>
2023-07-18 23:11:14 +0530

    v2/implementing spring data jpa operations details

--- springboot-rest-api/src/main/java/com/dailycodelearner/springboot/SpringbootRestApiApplication.java
+++ springboot-rest-api/src/main/java/com/dailycodelearner/springboot/SpringbootRestApiApplication.java
@@ -1,13 +1,26 @@
 package com.dailycodelearner.springboot;
 
+import org.apache.tomcat.util.net.jsse.JSSEUtil;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Supplier;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import static java.util.Arrays.stream;
 
 @SpringBootApplication
 public class SpringbootRestApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootRestApiApplication.class, args);
+
 	}
 
 }
--- springboot-rest-api/src/main/java/com/dailycodelearner/springboot/v3/bean/Student.java
+++ springboot-rest-api/src/main/java/com/dailycodelearner/springboot/v3/bean/Student.java
@@ -1,7 +1,6 @@
 package com.dailycodelearner.springboot.v3.bean;
 
 import lombok.*;
-import org.springframework.web.bind.annotation.GetMapping;
 
 @Data
 @ToString
